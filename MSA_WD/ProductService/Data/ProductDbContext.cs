9fc8c8e41fe27e22abee9c693e12ce4ae7871cc2
NavneetKumar6353 <navneet.kumar@rsystems.com>
2024-01-07 12:22:37 +0530

    API Createion, Gateway Creation & Integration of Gatteway with microservice

---
+++ MSA_WD/ProductService/Data/ProductDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Models;
+
+namespace ProductService.Data
+{
+    public class ProductDbContext:DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> option) : base(option)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
